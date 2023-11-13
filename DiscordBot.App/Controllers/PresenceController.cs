7cba1454e43714f4a6e54ab158cc2d82ce264f0a
Sxheeno <148947341+Sxheeno@users.noreply.github.com>
2023-11-13 07:26:55 +0300

    Updated PresenceController.cs
    Added Presence changes:
    -Replaced Activity type to be Roleplayers
    -Added allTimeHighPlayerCount but it's not 100% done because we need to fetch it initially and then update its value when triggered by the function. I commented how to do it, should take roughly 2 minutes to implement

--- DiscordBot.App/Controllers/PresenceController.cs
+++ DiscordBot.App/Controllers/PresenceController.cs
@@ -12,16 +12,38 @@ namespace DiscordBot.Controllers;
 public class PresenceController : ControllerBase
 {
     private readonly DiscordService _discordService;
+    // create a new variable in the database, instead of 400 this must be fetched from said database
+    private int allTimeHighPlayerCount = 400;
 
     public PresenceController(DiscordService discordService)
     {
         _discordService = discordService;
     }
 
+    private async Task SendMessageToGeneralChannel(string message)
+    {
+        DiscordChannel generalChannel = _discordService.GeneralChannel;
+        
+        if (generalChannel != null)
+        {
+            await generalChannel.SendMessageAsync(message);
+        }
+    }
+
     [HttpPost(Name = "players")]
     public async Task PostPlayers(int playerCount)
     {
         Log.Information("Post players");
-        await _discordService.Client.UpdateStatusAsync(new DiscordActivity($"with {playerCount} players!", ActivityType.Playing));
+        
+        if(playerCount > allTimeHighPlayerCount)
+        {
+            await _discordService.Client.UpdateStatusAsync(new DiscordActivity($"with a all time high of {allTimeHighPlayerCount} players!", ActivityType.Custom, ActivityProperties.None, "Roleplaying"));
+            //create post to replace allTimePlayerCount : allTimeHighPlayerCount = playerCount  
+            SendMessageToGeneralChannel($"We hit a all time peak of {allTimeHighPlayerCount} players!!!");   
+        }
+        else
+        {
+            await _discordService.Client.UpdateStatusAsync(new DiscordActivity($"with {playerCount} players!", ActivityType.Custom, ActivityProperties.None, "Roleplaying"));        
+        }
     }
 }
