29b756aa89b8cd950939785882069ac5a10b3bc7
Osvaldas Čiupkevičius <osvaldas.ciupkevicius@gmail.com>
2026-03-20 17:30:01 +0200

    Enabled the /login command to be used in DM.

--- DiscordBot.App/Commands/AuthenticationCommands.cs
+++ DiscordBot.App/Commands/AuthenticationCommands.cs
@@ -3,7 +3,7 @@ using DiscordBot.Apis;
 using DiscordBot.Services;
 using DSharpPlus.Commands;
 using DSharpPlus.Commands.Processors.SlashCommands;
-using DSharpPlus.Commands.Processors.SlashCommands.Metadata;
+using DSharpPlus.Commands.Trees.Metadata;
 using DSharpPlus.Entities;
 
 namespace DiscordBot.Commands;
@@ -21,7 +21,7 @@ public class AuthenticationCommands
 
     [Command("login")]
     [Description("Use to link your Discord user to an in-game account. Use via direct message only!")]
-    [InteractionAllowedContexts(DiscordInteractionContextType.BotDM)]
+    [AllowDMUsage]
     public async Task ExecuteAsync(SlashCommandContext ctx,
         [Parameter("account")] [Description("your in-game account name")] string username,
         [Parameter("password")] [Description("the password for your in-game account")] string password)
